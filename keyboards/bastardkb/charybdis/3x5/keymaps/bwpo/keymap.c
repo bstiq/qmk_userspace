784979aba824d75c98d35c745cf7fe6b154988ee
Quentin <bstkbd@gmail.com>
2025-09-02 12:15:46 +0200

    Revert "Update keymap.c"
    This reverts commit a37184f4b245e8a152f1ceb4d403294ae5f87caa.

--- keyboards/bastardkb/charybdis/3x5/keymaps/bwpo/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/bwpo/keymap.c
@@ -275,6 +275,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             SEND_STRING_DELAY("I hope this helps, let me know if there's anything else!",5);
         } 
         break;
+    }
     case QK_DOTMAJ:
         if (record->event.pressed) {
             tap_code(KC_DOT);
@@ -282,7 +283,5 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             OSM(MOD_LSFT);
         } 
         break;
-        
-    }
     return true;
 };
