af7b2615c848cd147114d9caefd5f04c31fae555
Quentin <bstkbd@gmail.com>
2025-09-02 11:48:20 +0200

    Update keymap.c

--- keyboards/bastardkb/charybdis/3x5/keymaps/bwpo/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/bwpo/keymap.c
@@ -280,7 +280,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         if (record->event.pressed) {
             tap_code(KC_DOT);
             tap_code(KC_SPACE);
-            tap_code(OS_LSFT);
+            OSM(MOD_LSFT);
         } 
         break;
     return true;
